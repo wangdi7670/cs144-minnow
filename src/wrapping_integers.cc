8be3de44facbb826d868e63df76274a2631635dc
WangDi <wangdi7670@163.com>
2023-11-16 15:41:23 +0000

    done wrap(), 但还没有测试它

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -7,7 +7,18 @@ Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
   // Your code here.
   (void)n;
   (void)zero_point;
-  return Wrap32 { 0 };
+
+  uint64_t size = 1L << 32;
+  uint64_t temp = size - zero_point.raw_value_;
+  uint32_t res = 0;
+
+  if (n < temp) {
+    res = zero_point.raw_value_ + n;
+  } else {
+    res = (n - temp) % size;
+  }
+
+  return Wrap32 { res };
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
