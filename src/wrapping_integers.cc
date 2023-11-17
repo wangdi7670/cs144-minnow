32dc37d1a8874261d155835c5386dca2c6746bb1
WangDi <wangdi7670@163.com>
2023-11-17 14:30:23 +0000

    初步完成 unwrap(), 测试用例还没有完全通过

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -1,4 +1,6 @@
 #include "wrapping_integers.hh"
+#include "stdint.h"
+#include <iostream>
 
 using namespace std;
 
@@ -26,5 +28,29 @@ uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
   // Your code here.
   (void)zero_point;
   (void)checkpoint;
-  return {};
+
+  uint64_t size = 1L << 32;
+  uint32_t index_bound = UINT32_MAX;  // 2^32 - 1
+  uint32_t ISN = zero_point.raw_value_;  // initial sequence number
+
+  uint64_t ab{};  // absolute number
+  if (ISN <= this->raw_value_ && this->raw_value_ <= index_bound) {
+    ab = this->raw_value_ - ISN;
+  } else if (this->raw_value_ < ISN) {
+    ab = this->raw_value_ + (size - ISN);
+  } else {
+    std::cout << "exception!" << "\n";
+  }
+
+  if (checkpoint <= ab) {
+    return ab;
+  }
+
+  while (!(ab <= checkpoint && checkpoint < ab + size)) {
+    ab += size;
+  }
+
+  ab = checkpoint - ab <= ab + size - checkpoint ? ab : ab + size;
+
+  return ab;
 }
