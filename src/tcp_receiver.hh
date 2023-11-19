b3ef8ac8b10eb7e71fc5986fdadbe19e480b2c12
WangDi <wangdi7670@163.com>
2023-11-19 02:00:06 +0000

    为TCPReceiver添加点成员变量：zero_point; valid; ackno

--- src/tcp_receiver.hh
+++ src/tcp_receiver.hh
@@ -3,9 +3,15 @@
 #include "reassembler.hh"
 #include "tcp_receiver_message.hh"
 #include "tcp_sender_message.hh"
+#include <optional>
 
 class TCPReceiver
 {
+private:
+  Wrap32 zero_point{0};  // initial sequence number
+  bool valid = false;  // whether ISN is valid
+  std::optional<Wrap32> ackno{};  
+
 public:
   /*
    * The TCPReceiver receives TCPSenderMessages, inserting their payload into the Reassembler
