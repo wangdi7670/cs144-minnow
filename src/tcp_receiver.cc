bdd9d5a43f4002bd065ec56b543f0046d81eb3d3
WangDi <wangdi7670@163.com>
2023-11-19 03:21:01 +0000

    send()和receive()初步完成，暂时还没通过payload不为空的测试用例

--- src/tcp_receiver.cc
+++ src/tcp_receiver.cc
@@ -8,11 +8,36 @@ void TCPReceiver::receive( TCPSenderMessage message, Reassembler& reassembler, W
   (void)message;
   (void)reassembler;
   (void)inbound_stream;
+
+  if (message.SYN) {
+    valid = true;
+    zero_point = message.seqno;
+  }
+
+  if (!valid) {
+    return;
+  }
+
+  uint64_t absolute_num = message.seqno.unwrap(zero_point, Wrap32::si2ab(reassembler.get_expected_index()));
+  uint64_t stream_index = message.SYN ? 0 : absolute_num - 1;
+  reassembler.insert(stream_index, (std::string&)message.payload, message.FIN, inbound_stream);
+
+  ackno = Wrap32::wrap(Wrap32::si2ab(reassembler.get_expected_index()), zero_point);
+  if (message.FIN) {
+    ackno = ackno.value() + 1;
+  }
+
+  if (message.FIN) {
+    valid = false;
+  }
 }
 
 TCPReceiverMessage TCPReceiver::send( const Writer& inbound_stream ) const
 {
   // Your code here.
   (void)inbound_stream;
-  return {};
+
+  uint64_t ac = inbound_stream.available_capacity();
+  uint16_t window = ac > UINT16_MAX ? UINT16_MAX : ac;
+  return TCPReceiverMessage{ackno, window};
 }
